e473405207cee33a57d3f5098ee38c95d4c81e68
Kasiian_Nataliia <natalakasian@gmail.com>
2025-09-30 21:35:01 +0300

    add arrays functions

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+function filterArray(numbers, value) {
+    let array = []
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+        if (number > value) {
+            array.push(number); 
+        }
+    }
+    return array;
+}
+
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
